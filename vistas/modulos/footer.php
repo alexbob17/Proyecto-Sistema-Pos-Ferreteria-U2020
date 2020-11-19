4ad56b95548bb27b61b40bed3072b2e681cd3912
alexbob17 <57636640+alexbob17@users.noreply.github.com>
2020-11-18 19:30:44 -0600

    Archivos completados

---
+++ vistas/modulos/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+	
+	<strong>Copyright &copy; 2020 <a href="Sistema de inventario" target="_blank">Uni Rupap</a>.</strong>
+
+	Todos los derechos reservados.
+
+
+</footer>
